2d9bd1b71d0036abf593cad64eb27649a43bf468
kaixa <690750353@qq.com>
2020-03-29 22:03:21 +0800

    试着用了用vue 但是失败了

---
+++ src/components/loginNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav role="navigation" class="navbar navbar-dark bg-warm-black" aria-label="top-level">
+        <a class="navbar-brand">BJUTLab</a>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="register.html">注册</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "loginNavbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const VuePlugin = require('vue-loader/lib/plugin-webpack4');
 const files = ['index', 'login', 'preorder', 'register', 'inform', 'me'];
 const getEntries = () => {
     const e = {};
@@ -14,7 +15,21 @@ module.exports = {
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, 'dist/js')
-
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: ['vue-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new VuePlugin()
+    ]
 
 };
