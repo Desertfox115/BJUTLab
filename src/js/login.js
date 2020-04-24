db4d843f1f222e2151bfbdea94d0a375207b7e17
Desertfox115 <rainbowno115@gmail.com>
2020-04-24 20:52:51 +0800

    添加记住密码 修改api服务器link

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,32 @@
 'use strict';
 import {serverLink} from './server';
+window.onload = function(){
+    //页面初始化时，如果cookie存在则填充
+    if (getCookie('username') && getCookie('password')) {
+        document.getElementById('username').value = getCookie('username');
+        document.getElementById('password').value = getCookie('password');
+        document.getElementById('remember').checked = true;
+    }
+};
+
+    //如果勾选状态发生改变，取消勾选时删除cookie
+    document.getElementById('remember').onchange = function() {
+        if (!this.checked) {
+            delCookie('username');
+            delCookie('password');
+        }
+    };
+    //表单提交时，如果勾选则保存cookie
+    document.getElementById('subLoginForm').onsubmit = function() {
+        if (document.getElementById('remember').checked) {
+            setCookie('username', oUser.value, 7);
+            setCookie('password', oPswd.value, 7);
+        }
+    };
+
 
 $('#login').on('click', () => {
+    document.getElementById('subLoginForm').submit();
     let loginFormData = new FormData();
     loginFormData.append('id', $('#school_id').val());
     loginFormData.append('password', $('#password').val());
@@ -9,12 +34,33 @@ $('#login').on('click', () => {
 
     fetch(serverLink + 'login', {
         method: 'POST',
-        body: loginFormData
+        body: loginFormData,
+        credentials:'include'
     }).then((res) => {
         if (res.ok) {
+
             window.location.href = 'index.html';
         }
     }).catch((e) => {
         console.log(e);
     });
 });
+
+
+function setCookie(name, value, day) {
+    let date = new Date();
+    date.setDate(date.getDate() + day);
+    document.cookie = name + '=' + value + ';expires=' + date;
+}
+function getCookie(name) {
+    let reg = RegExp(name + '=([^;]+)');
+    let arr = document.cookie.match(reg);
+    if (arr) {
+        return arr[1];
+    } else {
+        return '';
+    }
+}
+function delCookie(name) {
+    setCookie(name, null, -1);
+}
--- src/js/server.js
+++ src/js/server.js
@@ -1,3 +1,2 @@
-const debug = false;
 
-export const serverLink = debug ? 'http://127.0.0.1:5000/' : 'http://175.24.82.110/';
+export const serverLink = 'http://api.bjutlab.cn/';
